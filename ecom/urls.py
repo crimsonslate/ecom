0648843e9252b56fc99adbc4ca74be821b6acb5f
Blake Nall (darthnall) <dev@darthnall.com>
2024-09-01 18:24:12 -0500

    Reworked urls

--- ecom/urls.py
+++ ecom/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path("<str:slug>/", views.ProductDetailView.as_view(), name="product detail"),
     path("all/", views.ProductListView.as_view(), name="product list"),
     path("cart/", views.CartView.as_view(), name="cart"),
+    path("cart/add/<int:pk>/", views.CartView.as_view(), name="add to cart"),
+    path("cart/rm/<int:pk>/", views.CartView.as_view(), name="remove from cart"),
+    path("cart/clear/", views.CartView.as_view(), name="clear cart"),
 ]
